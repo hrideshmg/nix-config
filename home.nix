0df6a4b9371e580cfdd8d2208554765f6c4b2e31
Hridesh MG <hridesh699@gmail.com>
2026-04-30 15:44:29 +0530

    add tmux configuration

--- home.nix
+++ home.nix
@@ -27,6 +27,61 @@
 		};
 	};
 
+	programs.tmux = {
+		enable = true;
+
+		shortcut = "space";
+		baseIndex = 1;
+		keyMode = "vi";
+		mouse = true;
+		terminal = "tmux-256color";
+
+		extraConfig = ''
+		      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+		      set-option -g renumber-windows on
+		      unbind C-/
+
+		      # If only 1 pane, split it. If zoomed, unzoom. If multiple panes, toggle zoom.
+		      bind -n 'M-\' if-shell '[ "$(tmux list-panes | wc -l)" = 1 ]' 	\
+			'split-window -v -c "#{pane_current_path}" -l 25%'		\
+			'if-shell "[ -n \"$(tmux list-panes -F '''#F''' | grep Z)\" ]"	\
+			 "resize-pane -t1 -Z; select-pane -t2"				\
+			 "resize-pane -Z -t1"'
+
+		      # Window Navigation
+		      bind -n M-h previous-window
+		      bind -n M-l next-window
+
+		      # Quick Window Selection
+		      bind -n M-1 select-window -t 1
+		      bind -n M-2 select-window -t 2
+		      bind -n M-3 select-window -t 3
+		      bind -n M-4 select-window -t 4
+		      bind -n M-5 select-window -t 5
+
+		      # Splits and Session Management
+		      bind - split-window -v -c "#{pane_current_path}"
+		      bind \\ split-window -h -c "#{pane_current_path}"
+		      bind x kill-pane
+		      bind X kill-session
+		      set -g pane-active-border-style 'fg=#698DDA'
+
+		      # Pane Resizing (Prefix + Ctrl + hjkl)
+		      bind -r C-j resize-pane -D 2
+		      bind -r C-k resize-pane -U 2
+		      bind -r C-h resize-pane -L 2
+		      bind -r C-l resize-pane -R 2
+		    '';
+
+		sensibleOnTop = true;
+		plugins = with pkgs.tmuxPlugins; [
+			minimal-tmux-status
+			sensible
+			yank
+			vim-tmux-navigator
+		];
+	};
+
 	programs.git = {
 		enable = true;
 		settings = {
