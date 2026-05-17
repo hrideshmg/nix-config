4609e298dffe9aeac4fdc6b5fcdd051e5fbc79f1
Hridesh MG <hridesh699@gmail.com>
2026-05-17 14:31:40 +0530

    add windows machine to syncthing

--- nix_modules/syncthing.nix
+++ nix_modules/syncthing.nix
@@ -11,24 +11,37 @@
     openDefaultPorts = true;
     user = username;
     dataDir = "/home/${username}";
+    configDir = "/home/${username}/.config/syncthing";
     settings = {
       devices = {
         "Nothing 3A" = {
           id = "FI75FLT-7QXDIOE-SXNJFUR-4NL5DKY-WV4FKIJ-TZVUNHW-WJ3A262-6QY5FQX";
         };
+        "Windows PC" = {
+          id = "FSGTJ5K-F5BXUKI-WOCJEE3-UWLNPCR-WB4FRBZ-DZTVWU2-PR6HY2L-Q5PIVA3";
+        };
       };
       folders = {
         "obsidian" = {
           path = "/home/operator/documents/obsidian-vault";
-          devices = [ "Nothing 3A" ];
+          devices = [
+            "Nothing 3A"
+            "Windows PC"
+          ];
         };
         "keepass_sync" = {
           path = "/home/operator/documents/keepass_sync";
-          devices = [ "Nothing 3A" ];
+          devices = [
+            "Nothing 3A"
+            "Windows PC"
+          ];
         };
         "college" = {
           path = "/home/operator/college";
-          devices = [ "Nothing 3A" ];
+          devices = [
+            "Nothing 3A"
+            "Windows PC"
+          ];
         };
       };
     };
