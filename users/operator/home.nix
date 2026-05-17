455560ab0e169b02e13cbb9929337867d973b707
Hridesh MG <hridesh699@gmail.com>
2026-05-17 16:11:41 +0530

    move wayvnc config to operator

--- users/operator/home.nix
+++ users/operator/home.nix
@@ -1,8 +1,25 @@
-{ ... }:
+{ secrets, ... }:
 {
   imports = [
     ../../home_modules/base
     ../../home_modules/shell
     ../../home_modules/hyprland
+    ../../home_modules/hyprland/waybar
   ];
+
+  services.wayvnc = {
+    enable = true;
+    autoStart = true;
+    settings = {
+      address = "0.0.0.0";
+      port = 5900;
+      enable_auth = true;
+      relax_encryption = true;
+      use_relative_paths = true;
+      username = "operator";
+      password = secrets.wayvnc.password;
+      rsa_private_key_file = "${../../secrets/wayvnc/rsa_key.pem}";
+    };
+  };
+
 }
