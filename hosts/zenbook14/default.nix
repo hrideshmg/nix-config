ddf912df88f69f7169fa46050effc4ac10768b0e
Hridesh MG <hridesh699@gmail.com>
2026-05-22 20:46:26 +0530

    add bluetooth support

--- hosts/zenbook14/default.nix
+++ hosts/zenbook14/default.nix
@@ -4,6 +4,7 @@
     ../../nix_modules/system.nix
     ../../nix_modules/hyprland.nix
     ../../nix_modules/syncthing.nix
+    ../../nix_modules/bluetooth.nix
 
     ./hardware-configuration.nix
   ];
