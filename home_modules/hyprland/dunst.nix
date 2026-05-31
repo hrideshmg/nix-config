193396bed4977650d4e354e63cf9796fba86bf36
Hridesh MG <hridesh699@gmail.com>
2026-06-01 04:22:14 +0530

    fix pairing request

--- home_modules/hyprland/dunst.nix
+++ home_modules/hyprland/dunst.nix
@@ -1,4 +1,11 @@
 { ... }:
 {
-  services.dunst.enable = true;
+  services.mako.enable = true;
+
+  # Disable dunst from intercepting blueman messages
+  dconf.settings = {
+    "org/blueman/general" = {
+      notification-daemon = false;
+    };
+  };
 }
