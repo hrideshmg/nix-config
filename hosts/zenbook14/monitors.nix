3ccab470bc0b6dcbed3b23bea2b0cacd16e79d8d
Hridesh MG <hridesh699@gmail.com>
2026-09-07 11:10:49 +0530

    update main wallpaper

--- hosts/zenbook14/monitors.nix
+++ hosts/zenbook14/monitors.nix
@@ -4,7 +4,7 @@
     resolution = "2880x1800";
     position = "auto";
     refresh = "120";
-    wallpaper = "${../../wallpapers/anime_night_16_10.png}";
+    wallpaper = "${../../wallpapers/firewatch_oled.png}";
     scaling = "1.8";
   }
 
@@ -13,7 +13,7 @@
     resolution = "1920x1080";
     position = "auto-center-left";
     refresh = "60";
-    wallpaper = "${../../wallpapers/anime_night.png}";
+    wallpaper = "${../../wallpapers/firewatch_oled.png}";
     scaling = "1";
   }
 ]
